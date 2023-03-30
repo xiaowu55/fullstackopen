6666ec73fa811421f5c0ec13e478c4df8fb40ac6
jiazewu <jiazewu001@gmail.com>
2023-03-30 21:04:47 +0800

    1.12

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+
+const Button = props => <button onClick={props.onClick}>{props.name}</button>
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+
+  const getRandomInt = (min,max) =>{
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
+  const [selected, setSelected] = useState(0)
+
+  const clickHandle = () =>{
+    let num = getRandomInt(0,7)
+    console.log(num);
+    setSelected(num)
+  }
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <Button onClick={clickHandle}  name='next anecodote'/>
+    </div>
+  )
+}
+
+export default App
