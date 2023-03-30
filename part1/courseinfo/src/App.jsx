f0c3c53c6d4f9d71415019d5bf4eaa2e2a785aa6
jiazewu <jiazewu001@gmail.com>
2023-03-30 15:59:21 +0800

    1.6

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
-const Header = props => <h1>{props.title}</h1>
+const Header = props => <h1 >{props.title}</h1>
 
-const Button = props => <button>{props.name}</button>
+const Button = props => <button onClick={props.onClick}>{props.name}</button>
 
 const Content = props => <p>{props.name}  {props.degree}</p>
 
@@ -12,12 +12,27 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  console.log('re');
+
+  const clickHandle = (props) => { 
+    console.log(props);
+    if(props==='good'){
+      setGood(good+1)
+    }
+    else if(props==='neutral'){
+      setNeutral(neutral+1)
+    }
+    else{
+      setBad(bad+1)
+    }
+   }
+
   return (
     <div>
       <Header title='give feedback'/>
-      <Button name='good' />
-      <Button name='neutral' />
-      <Button name='bad' />
+      <Button name='good' onClick={()=>clickHandle('good')}/>
+      <Button name='neutral' onClick={()=>clickHandle('neutral')}/>
+      <Button name='bad' onClick={()=>clickHandle('bad')} />
       <Header title='statistics'/>
       <Content name='good' degree={good}/>
       <Content name='neutral' degree={neutral}/>
