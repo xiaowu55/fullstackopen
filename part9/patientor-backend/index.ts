c8fe001149fd6ace7013c57cd9021e1c6be0cb71
jiazewu <jiazewu001@gmail.com>
2023-05-03 00:29:27 +0800

    9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3001;
@@ -8,10 +10,9 @@ const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
-
 });
 
 app.listen(PORT, () => {
-    console.log('sever running on port');
+    console.log(`sever running on port ${PORT}`);
 
 });
